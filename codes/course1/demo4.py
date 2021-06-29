848745175d0c1de10cf9ab30c579f5a9950849d8
BigShuang <3508645145@qq.com>
2021-06-29 23:03:38 +0800

    complete 1-2

---
+++ codes/course1/demo4.py
@@ -0,0 +1,19 @@
+x = 1
+y = 2
+z = x + y
+print(str(x) + " + " + str(y) + " = " + str(z))
+
+x = 2
+y = 2
+z = x + y
+print(str(x) + " + " + str(y) + " = " + str(z))
+
+x = 3
+y = 4
+z = x + y
+print(str(x) + " + " + str(y) + " = " + str(z))
+
+x = 3
+y = 4
+z = x + y
+print("%s")
