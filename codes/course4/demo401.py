b3b56a3bc4997846d024da678f278d07bbfb170b
BigShuang <3508645145@qq.com>
2021-11-18 09:35:40 +0800

    update md

---
+++ codes/course4/demo401.py
@@ -0,0 +1,11 @@
+print("a", end=" ")
+print("b", end=" ")
+print("c", end=" ")
+print()
+print("-"*20)
+print(1, 2, 3, 4, sep="-")
+print("-"*20)
+words = list("abcdefg")
+
+for word in words:
+    print(word, end=",")
